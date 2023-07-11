78ca6dbec583cec83ad00e3ada28c9433f2f8cee
saadiaerr <saadiaerraiss1998@gmail.com>
2023-07-11 08:38:45 -0700

    task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Author
 for letter in range(97, 123):
-	if chr(letter) != 'e' and chr(letter) != 'q':
-		print("{}".format(chr(letter)), end="")
+    if chr(letter) != 'e' and chr(letter) != 'q':
+        print("{}".format(chr(letter)), end="")
