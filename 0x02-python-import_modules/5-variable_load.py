c45d4dfe29c3381a75c66bb7930af4faf94feb5d
saadiaerr <saadiaerraiss1998@gmail.com>
2023-07-13 06:38:04 -0700

    task5

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#program that imports the variable a from the file variable_load_5.py and prints its value.
+
+from variable_load_5 import a
+
+def find_var():
+    print(a)
+
+
+if __name__ == "__main__":
+    find_var()
