7b241748d7ccc729fe9866d2d3206ed32c42634b
saadiaerr <saadiaerraiss1998@gmail.com>
2023-07-13 06:40:09 -0700

    task5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#program that imports the variable a from the file variable_load_5.py and prints its value.
+#program that imports the variable a.
 
 from variable_load_5 import a
 
