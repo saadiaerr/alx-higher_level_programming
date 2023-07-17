da696194aa15f58369852123d157f7f26cad6781
saadiaerr <saadiaerraiss1998@gmail.com>
2023-07-17 14:52:07 -0700

    tjs

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
+
 def element_at(my_list, idx):
-    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
+    """
+     A function that retrieves an element from a list like in C.
+    """
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
+    return (my_list[idx])
