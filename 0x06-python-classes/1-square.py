9d4584118b4d2b545e6d4a5c00dfdc72a74d3d96
saadiaerr <saadiaerraiss1998@gmail.com>
2023-08-01 14:49:21 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class Square that definkles a square by: (based on 0-square.py)
+"""
+
+
+class Square():
+    """
+    class Square that defines ahiu square
+    """
+
+    def __init__(self, size):
+        self.__size = size
